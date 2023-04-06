0b066aed3d9a8726f875e46ce7cc0680639fa854
devrt2786 <rt2786@gmail.com>
2023-04-06 19:13:17 +0530

    design changes

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,5 +1,13 @@
 import { useState, useRef, useEffect } from "react";
-import { Box, Typography, Grid, Card, Button, IconButton } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Grid,
+  Card,
+  Button,
+  IconButton,
+  CircularProgress,
+} from "@mui/material";
 import {
   KeyboardVoice,
   Pause,
@@ -19,8 +27,11 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
   const [stream, setStream] = useState<any>(null);
   const [audio, setAudio] = useState<any>(null);
   const [audioFile, setAudioFile] = useState<any>(null);
+  const [recAudio, setRecAudio] = useState<any>(null);
   const [audioChunks, setAudioChunks] = useState<any[]>([]);
   const [pauseRecordingStatus, setPauseRecordingStatus] = useState("");
+  const [loader, setLoader] = useState(false);
+  const [upLoader, setUpLoader] = useState(false);
   const getMicrophonePermission = async () => {
     if ("MediaRecorder" in window) {
       try {
@@ -39,25 +50,27 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
   };
 
   const startRecording = async () => {
-    setRecordingStatus("recording");
-    let options: any = {
-      type: mimeType,
-    };
-    const media: any = new MediaRecorder(stream, options);
+    if (permission) {
+      setRecordingStatus("recording");
+      let options: any = {
+        type: mimeType,
+      };
+      const media: any = new MediaRecorder(stream, options);
 
-    mediaRecorder.current = media;
+      mediaRecorder.current = media;
 
-    mediaRecorder.current.start();
+      mediaRecorder.current.start();
 
-    let localAudioChunks: any[] = [];
+      let localAudioChunks: any[] = [];
 
-    mediaRecorder.current.ondataavailable = (event: any) => {
-      if (typeof event.data === "undefined") return;
-      if (event.data.size === 0) return;
-      localAudioChunks.push(event.data);
-    };
+      mediaRecorder.current.ondataavailable = (event: any) => {
+        if (typeof event.data === "undefined") return;
+        if (event.data.size === 0) return;
+        localAudioChunks.push(event.data);
+      };
 
-    setAudioChunks(localAudioChunks);
+      setAudioChunks(localAudioChunks);
+    }
   };
 
   const pauseRecording = () => {
@@ -86,7 +99,9 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
       setAudioChunks([]);
     };
   };
-
+  useEffect(() => {
+    getMicrophonePermission();
+  }, []);
   useEffect(() => {
     (async () => {
       if (audio) {
@@ -98,7 +113,8 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
                 type: mimeType,
               })
           );
-        setAudioFile(recFile);
+        // setAudioFile(recFile);
+        setRecAudio(recFile);
         console.log("file", recFile);
       }
     })();
@@ -106,12 +122,12 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
       // this now gets called when the component unmounts
     };
   }, [audio]);
-  console.log("audio", audio);
   const handleFileChange = (file: any) => {
-    console.log("file", file);
     setAudioFile(file[0]);
   };
   const handleUpload = async () => {
+    setLoader(true);
+
     let data: any = new FormData();
     data.append("audio", audioFile, audioFile?.name);
     let uploadedRes: any = await axios({
@@ -125,33 +141,75 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
       const { data } = uploadedRes;
 
       transcribeFuc(data?.secure_url);
+      setLoader(false);
     } else {
+      setLoader(false);
     }
   };
+  const handleRecUpload = async () => {
+    setUpLoader(true);
 
+    let data: any = new FormData();
+    data.append("audio", recAudio, recAudio?.name);
+    let uploadedRes: any = await axios({
+      method: "post",
+      url: "https://dark-plum-caterpillar-ring.cyclic.app/audio/upload",
+      data,
+    })
+      .then((response) => response)
+      .catch((err) => err.response);
+    if (uploadedRes.status === 200) {
+      const { data } = uploadedRes;
+
+      transcribeFuc(data?.secure_url);
+      setUpLoader(false);
+    } else {
+      setUpLoader(false);
+    }
+  };
   return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <Box>
-        <Typography
-          variant="inherit"
-          sx={{
-            fontSize: "50px",
-            fontWeight: "bold",
-            mt: 5,
-            textAlign: "center",
-          }}
-        >
-          Otto - Biography
-        </Typography>
+    <>
+      <Typography
+        variant="inherit"
+        sx={{
+          fontSize: "50px",
+          fontWeight: "bold",
+          mt: 5,
+          textAlign: "center",
+        }}
+      >
+        Otto - Biography
+      </Typography>
+
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          pl: 25,
+          gap: 2,
+          pr: 25,
+          mt: 7,
+        }}
+      >
         <Box
           sx={{
+            p: 3,
+            pl: 10,
+            pr: 10,
             display: "flex",
-            justifyContent: "center",
-
-            mt: 10,
+            flexDirection: "column",
+            background: "	#e6e6e6",
+            border: "1px solid 	#D3D3D3",
+            borderRadius: "20px",
           }}
         >
-          {!permission ? (
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", fontSize: "30px", fontWeight: "600" }}
+          >
+            Record voice note
+          </Typography>
+          {/* {!permission ? (
             <Button
               variant="contained"
               color="warning"
@@ -160,114 +218,177 @@ const AudioRecorder = ({ transcribeFuc }: any) => {
             >
               Connect Microphone
             </Button>
-          ) : null}
-          {permission ? (
-            <IconButton onClick={startRecording}>
-              <KeyboardVoice
-                sx={{
-                  fontSize: "70px",
-                  color: recordingStatus !== "inactive" ? "green" : "",
-                }}
-              />
-            </IconButton>
-          ) : null}
-        </Box>
+          ) : null} */}
 
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          {permission ? (
-            // <button onClick={stopRecording} type="button">
-            //   Stop Recording
-            // </button>
-            <>
-              <IconButton onClick={stopRecording}>
-                <StopCircle
-                  sx={{
-                    fontSize: "20px",
-                    color: recordingStatus !== "inactive" ? "red" : "",
-                  }}
-                />
-              </IconButton>
+          <IconButton
+            onClick={startRecording}
+            disabled={loader ? true : false}
+            sx={{
+              alignItems: "center",
+              // "&.css-1yqn7wg-MuiButtonBase-root-MuiIconButton-root": {
+              //   background: "none !important",
+              // },
+              "& .css-1yqn7wg-MuiButtonBase-root-MuiIconButton-root": {
+                background: "none !important",
+              },
+            }}
+          >
+            <KeyboardVoice
+              sx={{
+                fontSize: "70px",
+                alignItems: "center",
+                color: recordingStatus !== "inactive" ? "green" : "",
+              }}
+            />
+          </IconButton>
 
-              {recordingStatus === "inactive" ? (
-                <IconButton onClick={resumeRecording}>
-                  <PlayArrow
-                    sx={{
-                      fontSize: "20px",
-                      color: recordingStatus !== "inactive" ? "green" : "",
-                    }}
-                  />
-                </IconButton>
-              ) : (
-                <IconButton onClick={pauseRecording}>
-                  <Pause
-                    sx={{
-                      fontSize: "20px",
-                      color: recordingStatus !== "inactive" ? "green" : "",
-                    }}
-                  />
-                </IconButton>
-              )}
-            </>
-          ) : null}
-        </Box>
-
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            mt: 4,
-            flexDirection: "column",
-          }}
-        >
-          {/* <audio src={audio} controls></audio> */}
-          {audio ? (
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <audio src={audio} controls></audio>
-            </Box>
-          ) : null}
           <Box
             sx={{
               display: "flex",
+              flexDirection: "column",
               alignItems: "center",
-              justifyContent: "center",
-              gap: 3,
-              mb: 3,
             }}
           >
-            <Button variant="contained" color="warning" onClick={handleUpload}>
+            {permission ? (
+              // <button onClick={stopRecording} type="button">
+              //   Stop Recording
+              // </button>
+              <>
+                <Box>
+                  <IconButton
+                    onClick={stopRecording}
+                    disabled={loader ? true : false}
+                  >
+                    <StopCircle
+                      sx={{
+                        fontSize: "25px",
+                        color: recordingStatus !== "inactive" ? "red" : "",
+                      }}
+                    />
+                  </IconButton>
+
+                  {recordingStatus === "inactive" ? (
+                    <IconButton
+                      onClick={resumeRecording}
+                      disabled={loader ? true : false}
+                    >
+                      <PlayArrow
+                        sx={{
+                          fontSize: "25px",
+                          color: recordingStatus !== "inactive" ? "green" : "",
+                        }}
+                      />
+                    </IconButton>
+                  ) : (
+                    <IconButton onClick={pauseRecording}>
+                      <Pause
+                        sx={{
+                          fontSize: "25px",
+                          color: recordingStatus !== "inactive" ? "green" : "",
+                        }}
+                      />
+                    </IconButton>
+                  )}
+                </Box>
+              </>
+            ) : null}
+
+            {audio ? <audio src={audio} controls></audio> : null}
+
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ mt: 2, borderRadius: "30px " }}
+              disabled={loader ? true : false}
+              onClick={handleRecUpload}
+            >
               <Transcribe sx={{ mr: 1 }} />
-              Transcribe
+              {upLoader ? (
+                <CircularProgress size="20px" sx={{ ml: 3, color: "#fff" }} />
+              ) : (
+                <>Transcribe</>
+              )}
             </Button>
 
-            <Button variant="contained" color="warning">
-              <input
-                type="file"
-                onChange={(e) => handleFileChange(e.target.files)}
-                style={{ width: "200px" }}
-              />
-              Upload Audio
-              <CloudUpload sx={{ ml: 1, fontSize: "20px" }} />
-            </Button>
-          </Box>
-          {audio ? (
-            <Button variant="contained" color="success">
-              <a
-                style={{
-                  textDecoration: "none",
-                  color: "#fff",
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                download
-                href={audio}
+            {audio ? (
+              <Button
+                variant="contained"
+                color="success"
+                sx={{ mt: 2, borderRadius: "30px " }}
               >
-                Download Recording <Download sx={{ ml: 2, fontSize: "20px" }} />
-              </a>
-            </Button>
-          ) : null}
+                <a
+                  style={{
+                    textDecoration: "none",
+                    color: "#fff",
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                  download
+                  href={audio}
+                >
+                  Download Recording{" "}
+                  <Download sx={{ ml: 2, fontSize: "20px" }} />
+                </a>
+              </Button>
+            ) : null}
+          </Box>
+        </Box>
+        <Box>
+          <Typography variant="h4" sx={{ fontSize: "30px", mt: 15 }}>
+            OR
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            background: "	#e6e6e6",
+            border: "1px solid 	#D3D3D3",
+            borderRadius: "20px",
+            p: 3,
+            pl: 8,
+            pr: 8,
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{ textAlign: "center", fontSize: "30px", fontWeight: "600" }}
+          >
+            Upload voice note
+          </Typography>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ mt: 3, borderRadius: "30px " }}
+          >
+            <input
+              type="file"
+              disabled={upLoader ? true : false}
+              onChange={(e) => handleFileChange(e.target.files)}
+              style={{ width: "200px" }}
+            />
+          </Button>
+
+          <Button
+            sx={{
+              mt: 3,
+              borderRadius: "30px ",
+            }}
+            disabled={upLoader ? true : false}
+            variant="contained"
+            color="success"
+            onClick={handleUpload}
+          >
+            <Transcribe sx={{ mr: 1 }} />
+            {loader ? (
+              <CircularProgress size="20px" sx={{ ml: 3, color: "#fff" }} />
+            ) : (
+              <>Transcribe</>
+            )}
+          </Button>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
